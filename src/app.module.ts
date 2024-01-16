3cb2edb4100d0169eb7141fef301150d20ed444e
abdelrhamanZ <abdelrahman.m.fcih@gmail.com>
2024-01-16 23:42:07 +0200

    feat date

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [HttpModule, ScheduleModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,29 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { AxiosResponse } from 'axios';
+import { Observable, lastValueFrom } from 'rxjs';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly httpService: HttpService) {}
   private readonly logger = new Logger(AppService.name);
-  getHello() {
-    return new Date(Date.now());
+  async getHello() {
+    const response = await axios.get(
+      'https://backend.bazaarmyr.com/v1/testDeploy',
+    );
+    // Assuming the current date and time
+    const currentUTCDate = new Date();
+
+    // Set the time zone to Cairo (Egypt Standard Time)
+    const options = { timeZone: 'Africa/Cairo' };
+
+    // Format the date according to the user's locale
+    // const cairoDate = currentUTCDate.toLocaleString('en-US', options);
+    const cairoDate = currentUTCDate.toLocaleString('ar-EG', options);
+    return { data: response.data, date: cairoDate };
+    //return ;
   }
 
   @Cron(CronExpression.EVERY_MINUTE)
